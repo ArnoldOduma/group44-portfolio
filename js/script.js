b6523525325e89a499d763caa6a938ca17959d0b
ArnoldOduma <arnoldcliff98@gmail.com>
2022-08-09 15:40:40 +0300

    Addition of hero sections and about section

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("sidebar").style.height = "auto";
+    document.getElementById("sidebar").style.padding = "30px 0";
+
+}
+
+function closeNav() {
+    document.getElementById("sidebar").style.height = "0";
+    document.getElementById("sidebar").style.padding = "0";
+
+}
